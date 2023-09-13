f1c52c26e8490b8e13e8e30959d8718246b667ce
M. Imam Renaldy Gumay <imam.renaldy26@gmail.com>
2023-09-13 19:43:18 +0700

    init dependecy

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
