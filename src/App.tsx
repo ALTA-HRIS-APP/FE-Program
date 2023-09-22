023a9d89c9c7e93634ec4ed2bc9c0bbfa30087a0
YusufPandego253 <138494122+YusufPandego253@users.noreply.github.com>
2023-09-22 19:15:11 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,15 +19,12 @@ import AddUser from "./pages/UserManagement/AddUser";
 import DetailUser from "./pages/UserManagement/DetailUser";
 import EditUser from "./pages/UserManagement/EditUser";
 import EmployeeLevel from "./pages/EmployeeLevel/indexEmployeeLevel";
-<<<<<<< HEAD
 import AddEmployeeLevel from "./pages/EmployeeLevel/AddEmployeeLevel";
 import AddTimeOff from "./pages/TimeOff/AddTimeOff";
-=======
 import EditEmployeeLevel from "./pages/EmployeeLevel/EditEmployeeLevel";
 import IndexDevisi from "./pages/DevisiManagement/IndexDevisi";
 import TambahDevisi from "./pages/DevisiManagement/TambahDevisi";
 import EditDevisi from "./pages/DevisiManagement/EditDevisi";
->>>>>>> a9f36fb5be0802198530e269c50fb87a39fd3724
 
 export default function App() {
   axios.defaults.baseURL = "https://project2.otixx.online/";
@@ -54,13 +51,11 @@ export default function App() {
           <Route path="/Employe" element={<Employe />} />
           {/* <Route path="/AddEmploye" element={<AddEmploye />} /> */}
           <Route path="/Profile" element={<Profile />} />
-<<<<<<< HEAD
           <Route path="/AddTimeOff" element={<AddTimeOff />} />
-=======
           <Route path="/Devisi" element={<IndexDevisi />} />
           <Route path="/TambahDevisi" element={<TambahDevisi />} />
           <Route path="/EditDevisi/:id" element={<EditDevisi />} />
->>>>>>> a9f36fb5be0802198530e269c50fb87a39fd3724
+
         </Routes>
       </Layout>
     </BrowserRouter>
